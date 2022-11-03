a3deb73fb2f070de75b9028eee3c07df2ca7c38b
Thiago da Silva Moreira <thiagosm.004@gmail.com>
2022-11-03 00:02:13 -0300

    Develop the interfaces with the contract's method. Fixing the error when instanciate a new IBreakfastService and testing the endpoints.

--- BuberBreakfast/Services/Breakfasts/BreakfastService.cs
+++ BuberBreakfast/Services/Breakfasts/BreakfastService.cs
@@ -5,7 +5,7 @@ namespace BuberBreakfast.Services.Breakfasts
     public class BreakfastService : IBreakfastService
     {
         // here we should develop the methods to store the data in the database with some ORM like entity framework.
-        // But for simplified, the data will be store in a Dict varible
+        // But for simplified, the data will be store in a Dict varible 
         public static readonly Dictionary<Guid, Breakfast> _breakfasts = new Dictionary<Guid, Breakfast>();
 
         public void CreateBreakfast(Breakfast breakfasts)
